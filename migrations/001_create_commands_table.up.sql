638efdbe867f7b8f0c9a32a89408aa12c7c73aba
Islam <islam.sh.r@icloud.com>
2024-03-13 12:17:19 +0300

    changed

--- migrations/001_create_commands_table.up.sql
+++ migrations/001_create_commands_table.up.sql
@@ -1,5 +1,5 @@
 -- migrations/001_create_commands_table.sql
 CREATE TABLE IF NOT EXISTS commands (
-    id SERIAL PRIMARY KEY,
+    id SERIAL NOT NULL constraint PK_commands PRIMARY KEY,
     content TEXT
 );
